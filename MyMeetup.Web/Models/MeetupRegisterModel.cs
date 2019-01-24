6b4353c3c998003820628b042594b3110c2f852a
Guillaume Jay <guillaumejay@users.noreply.github.com>
2019-01-24 06:47:15 +0100

    Strating work on registration

---
+++ MyMeetup.Web/Models/MeetupRegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyMeetUp.Logic.Entities;
+
+namespace MyMeetup.Web.Models
+{
+    public class MeetupRegisterModel
+    {
+        public Meetup Meetup { get; set; }
+
+        public int AdultNumber { get; set; }
+
+        public int ChildrenNumber { get; set; }
+
+        public string AccomodationChoice { get; set; }
+
+        public string Notes { get; set; }
+        public List<SelectListItem> PossibleAccomodations{ get; set; }
+
+        public string AccomodationId { get; set; }
+    }
+}
