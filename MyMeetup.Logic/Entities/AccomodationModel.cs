51a5387ac77946b7b1c8ea1ee6822ea4bf48053e
Guillaume Jay <guillaumejay@users.noreply.github.com>
2019-02-20 20:35:05 +0100

    Accomodation update (yes, it's wrong)

--- MyMeetup.Logic/Entities/AccomodationModel.cs
+++ MyMeetup.Logic/Entities/AccomodationModel.cs
@@ -32,10 +32,10 @@ public    class AccomodationModel
                 {
                     new AccomodationModel("Camping", "Emplacement Camping", true),
                     new AccomodationModel("Landette4", "Landette 4 couchages (165€)", true),
-                    new AccomodationModel("Landette6", "Landette 6 couchages (165€)", true),
-                    new AccomodationModel("Chalet", "Chalet (3-4 places, 280€)", true),
-                    new AccomodationModel("Hacienda", "Mobilhome Hacienda (8 places, 340€)", true),
-                    new AccomodationModel("Yourte", "Yourte (4-6 places / 2 chambres : 340€)", true),
+                    new AccomodationModel("Landette6", "Landette 6 couchages (165€)", false),
+                    new AccomodationModel("Chalet", "Chalet (3-4 places, 280€)", false),
+                    new AccomodationModel("Hacienda", "Mobilhome Hacienda (8 places, 340€)", false),
+                    new AccomodationModel("Yourte", "Yourte (4-6 places / 2 chambres : 340€)", false),
                     new AccomodationModel("GiteT4", "Gîte T4 (8 places, 390€)", false),
                     new AccomodationModel("GiteT6", "Chambre dans Gîte T6 (212€)", true)
                 };
