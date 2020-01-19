23ffa1551f485999d04acccb515d8759e2a5a004
Guillaume Jay <guillaume.jay@gmail.com>
2020-01-19 17:50:09 +0100

    Accomodations corrected

--- MyMeetup.Logic/Entities/AccomodationModel.cs
+++ MyMeetup.Logic/Entities/AccomodationModel.cs
@@ -5,21 +5,21 @@ using System.Text;
 
 namespace MyMeetUp.Logic.Entities
 {
-public    class AccomodationModel
-{
-    public AccomodationModel()
+    public class AccomodationModel
+    {
+        public AccomodationModel()
         { }
 
-    public AccomodationModel(string id, string description, bool isActive)
-    {
-        Id = id;
-        Description = description;
-        IsActive = isActive;
-    }
+        public AccomodationModel(string id, string description, bool isActive)
+        {
+            Id = id;
+            Description = description;
+            IsActive = isActive;
+        }
         [Key]
         public string Id { get; set; }
 
-        
+
         public string Description { get; set; }
 
         public bool IsActive { get; set; }
@@ -31,15 +31,16 @@ public    class AccomodationModel
                 var accomodations = new List<AccomodationModel>
                 {
                     new AccomodationModel("Camping", "Emplacement Camping", true),
-                    new AccomodationModel("Docinela", "Mobilhome Docinela (4 places, 2 chambres) 270€", true),
-                    new AccomodationModel("Pichonela", "Mobilhome Pichonela (5 places, 2 chambres) 280€", false),
-                    new AccomodationModel("Landette1", "Landette 1 couchage (avec sanitaires) (165€)", false),
-                    new AccomodationModel("Landette46", "Landettes (4 ou 6 places, une seule pièce) 165€", true),
-                    new AccomodationModel("ChaletStudios", "Chalets/studio (3-4 places) 280€", false),
-                    new AccomodationModel("Hacienda", "Mobilhome Hacienda (8 places) 340€", true),
-                    new AccomodationModel("Yourte", "Yourte (4-6 places, 2 chambres) 340€)", true),
-                    new AccomodationModel("GiteT4", "Gîte T4 (8 places) 390€", false),
-                    new AccomodationModel("GiteT6", "Chambre dans Gîte T6 (2-3 lits, avec douche/wc privés) 212€", true)
+                                         new AccomodationModel("Docinela", "Mobilhome Docinela (4 places, 2 chambres) 270€", true),
+                                         new AccomodationModel("Pichonela", "Mobilhome Pichonela (5 places, 2 chambres) 280€", true),
+                                         new AccomodationModel("Landette1", "Landette 1 couchage (avec sanitaires) (165€)", true),
+                                         new AccomodationModel("Landette46", "Landettes (4 ou 6 places, une seule pièce) 165€", true),
+                                         new AccomodationModel("ChaletStudios",   "Chalets/studio (3-4 places) 280€", true),
+                                         new AccomodationModel("Hacienda", "Mobilhome Hacienda (8 places) 340€", true),
+                                 new AccomodationModel("Yourte", "Yourte (4-6 places, 2 chambres) 340€)", true),
+                                       new AccomodationModel("GiteT4", "Gîte T4 (8 places, 3 chambres) 390€", true),
+                                        new AccomodationModel("Cabanela", "Cabanela (4 places, 2 chambres) 350€", true),
+                                                     new AccomodationModel("GiteT6", "Chambre dans Gîte T6 (2-3 lits, avec douche/wc privés) 212€", true)
                 };
 
                 return accomodations;
