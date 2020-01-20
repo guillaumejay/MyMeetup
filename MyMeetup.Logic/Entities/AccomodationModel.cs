1bee6e27d1bd5efcaa6516847874f2ad2dda980e
Guillaume Jay <guillaume.jay@gmail.com>
2020-01-20 21:19:47 +0100

    Various updates

--- MyMeetup.Logic/Entities/AccomodationModel.cs
+++ MyMeetup.Logic/Entities/AccomodationModel.cs
@@ -31,15 +31,15 @@ namespace MyMeetUp.Logic.Entities
                 var accomodations = new List<AccomodationModel>
                 {
                     new AccomodationModel("Camping", "Emplacement Camping", true),
-                                         new AccomodationModel("Docinela", "Mobilhome Docinela (4 places, 2 chambres) 270€", true),
+                                         new AccomodationModel("Docinela", "Mobilhome Docinela (4 places, 2 chambres) 270€", false),
                                          new AccomodationModel("Pichonela", "Mobilhome Pichonela (5 places, 2 chambres) 280€", true),
                                          new AccomodationModel("Landette1", "Landette 1 couchage (avec sanitaires) (165€)", true),
-                                         new AccomodationModel("Landette46", "Landettes (4 ou 6 places, une seule pièce) 165€", true),
-                                         new AccomodationModel("ChaletStudios",   "Chalets/studio (3-4 places) 280€", true),
-                                         new AccomodationModel("Hacienda", "Mobilhome Hacienda (8 places) 340€", true),
+      new AccomodationModel("Landette46", "Landettes (4 ou 6 places, une seule pièce) 165€", true),
+           new AccomodationModel("ChaletStudios",   "Chalets/studio (3-4 places) 280€", true),
+          new AccomodationModel("Hacienda", "Mobilhome Hacienda (8 places) 340€", false),
                                  new AccomodationModel("Yourte", "Yourte (4-6 places, 2 chambres) 340€)", true),
                                        new AccomodationModel("GiteT4", "Gîte T4 (8 places, 3 chambres) 390€", true),
-                                        new AccomodationModel("Cabanela", "Cabanela (4 places, 2 chambres) 350€", true),
+                                        new AccomodationModel("Cabanela", "Cabanela (4 places, 2 chambres) 350€", false),
                                                      new AccomodationModel("GiteT6", "Chambre dans Gîte T6 (2-3 lits, avec douche/wc privés) 212€", true)
                 };
 
